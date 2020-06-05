497090b12163ccf0a24bb151170ead0f56f17dc8
bixilon <bixilon@bixilon.de>
2020-06-05 19:52:28 +0200

    fix wrong game mode number

--- src/main/java/de/bixilon/minosoft/game/datatypes/GameMode.java
+++ src/main/java/de/bixilon/minosoft/game/datatypes/GameMode.java
@@ -3,8 +3,8 @@ package de.bixilon.minosoft.game.datatypes;
 public enum GameMode {
     SURVIVAL(0),
     CREATIVE(1),
-    ADVENTURE(3),
-    SPECTATOR(4);
+    ADVENTURE(2),
+    SPECTATOR(3);
 
     int id;
 
