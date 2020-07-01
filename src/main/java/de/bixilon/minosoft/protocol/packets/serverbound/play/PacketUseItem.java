8527f15a9838fb0eef4d8691c84543dc4d9f4c59
Bixilon <bixilon@bixilon.de>
2020-07-01 22:34:39 +0200

    PacketUseItem

---
+++ src/main/java/de/bixilon/minosoft/protocol/packets/serverbound/play/PacketUseItem.java
@@ -0,0 +1,47 @@
+/*
+ * Codename Minosoft
+ * Copyright (C) 2020 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  This software is not affiliated with Mojang AB, the original developer of Minecraft.
+ */
+
+package de.bixilon.minosoft.protocol.packets.serverbound.play;
+
+import de.bixilon.minosoft.game.datatypes.player.Hand;
+import de.bixilon.minosoft.logging.Log;
+import de.bixilon.minosoft.protocol.packets.ServerboundPacket;
+import de.bixilon.minosoft.protocol.protocol.OutPacketBuffer;
+import de.bixilon.minosoft.protocol.protocol.Packets;
+import de.bixilon.minosoft.protocol.protocol.ProtocolVersion;
+
+public class PacketUseItem implements ServerboundPacket {
+    Hand hand;
+
+    public PacketUseItem(Hand hand) {
+        this.hand = hand;
+        log();
+    }
+
+
+    @Override
+    public OutPacketBuffer write(ProtocolVersion version) {
+        OutPacketBuffer buffer = new OutPacketBuffer(version, version.getPacketCommand(Packets.Serverbound.PLAY_USE_ITEM));
+        switch (version) {
+            case VERSION_1_9_4:
+                buffer.writeVarInt(hand.getId());
+                break;
+        }
+        return buffer;
+    }
+
+    @Override
+    public void log() {
+        Log.protocol(String.format("Use hand (hand=%s)", hand.name()));
+    }
+}
