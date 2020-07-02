7bdf27311554eb52c2720748f30ea83668288b5b
Bixilon <bixilon@bixilon.de>
2020-07-02 22:32:19 +0200

    PacketSteerBoat

---
+++ src/main/java/de/bixilon/minosoft/protocol/packets/serverbound/play/PacketSteerBoat.java
@@ -0,0 +1,51 @@
+/*
+ * Codename Minosoft
+ * Copyright (C) 2020 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  This software is not affiliated with Mojang AB, the original developer of Minecraft.
+ */
+
+package de.bixilon.minosoft.protocol.packets.serverbound.play;
+
+import de.bixilon.minosoft.logging.Log;
+import de.bixilon.minosoft.protocol.packets.ServerboundPacket;
+import de.bixilon.minosoft.protocol.protocol.OutPacketBuffer;
+import de.bixilon.minosoft.protocol.protocol.Packets;
+import de.bixilon.minosoft.protocol.protocol.ProtocolVersion;
+
+public class PacketSteerBoat implements ServerboundPacket {
+
+    final boolean leftPaddle;
+    final boolean rightPaddle;
+
+
+    public PacketSteerBoat(boolean leftPaddle, boolean rightPaddle) {
+        this.leftPaddle = leftPaddle;
+        this.rightPaddle = rightPaddle;
+        log();
+    }
+
+
+    @Override
+    public OutPacketBuffer write(ProtocolVersion version) {
+        OutPacketBuffer buffer = new OutPacketBuffer(version, version.getPacketCommand(Packets.Serverbound.PLAY_STEER_BOAT));
+        switch (version) {
+            case VERSION_1_9_4:
+                buffer.writeBoolean(leftPaddle);
+                buffer.writeBoolean(rightPaddle);
+                break;
+        }
+        return buffer;
+    }
+
+    @Override
+    public void log() {
+        Log.protocol(String.format("Steering boat: (leftPaddle=%s, rightPaddle=%s)", leftPaddle, rightPaddle));
+    }
+}
