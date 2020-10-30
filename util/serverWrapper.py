74d9d335517e244647b831d547978c793b2b3fa8
Bixilon <bixilon@bixilon.de>
2020-10-30 14:47:15 +0100

    fix copyright in python scripts, fix python formatting

--- util/serverWrapper.py
+++ util/serverWrapper.py
@@ -10,16 +10,6 @@
 *
 *  This software is not affiliated with Mojang AB, the original developer of Minecraft.
 """
-#  Minosoft
-#  Copyright (C) 2020 Moritz Zwerger
-#
-#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#   This software is not affiliated with Mojang AB, the original developer of Minecraft.
 
 import os
 import requests
@@ -69,7 +59,7 @@ if len(sys.argv) > 1:
 
 while True:
     version = input('Enter a minecraft version: ')
-    if len(version) < 2 or len(version) > 12 or not version.find("/"):
+    if len(version) < 2 or not version.find("/") or not version.find("."):
         print("Invalid version!")
         continue
     if not os.path.isfile("./" + version + "/server.jar"):
@@ -97,4 +87,4 @@ while True:
         print("CTRL + C")
         process.kill()
 
-    print("Server stopped (" + str(process.returncode) + ")")
+    print("Server stopped (%d)" % str(process.returncode))
