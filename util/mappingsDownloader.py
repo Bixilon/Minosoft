6ef561dd69b0c5cf56b0d04806a584bf7cec37fa
Bixilon <bixilon@bixilon.de>
2020-11-05 21:38:21 +0100

    Fix copyright spacing

--- util/mappingsDownloader.py
+++ util/mappingsDownloader.py
@@ -4,24 +4,30 @@
 *
 * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 *
-*  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 *
-*  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
-*  This software is not affiliated with Mojang AB, the original developer of Minecraft.
+* This software is not affiliated with Mojang AB, the original developer of Minecraft.
 """
-import \
-    os
-import \
-    requests
-import \
-    shutil
-import \
-    tarfile
-import \
-    traceback
-import \
-    ujson
+
+#  Minosoft
+#  Copyright (C) 2020 Moritz Zwerger
+#
+#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with this program.If not, see <https://www.gnu.org/licenses/>.
+#
+#  This software is not affiliated with Mojang AB, the original developer of Minecraft.
+
+import os
+import requests
+import shutil
+import tarfile
+import traceback
+import ujson
 
 print("Minecraft mappings downloader (and generator)")
 
