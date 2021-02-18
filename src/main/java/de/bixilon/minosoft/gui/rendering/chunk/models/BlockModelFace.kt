85e0c5a95a55aa3c3ef9e6d68114fe788db507bf
Bixilon <bixilon@bixilon.de>
2021-02-18 21:20:09 +0100

    copyright: add copyright header to all new files

--- src/main/java/de/bixilon/minosoft/gui/rendering/chunk/models/BlockModelFace.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/chunk/models/BlockModelFace.kt
@@ -1,3 +1,16 @@
+/*
+ * Minosoft
+ * Copyright (C) 2020 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not affiliated with Mojang AB, the original developer of Minecraft.
+ */
+
 package de.bixilon.minosoft.gui.rendering.chunk.models
 
 import com.google.gson.JsonObject
@@ -7,7 +20,7 @@ import glm_.vec2.Vec2
 class BlockModelFace(data: JsonObject) {
     val textureName: String = data.get("texture").asString.removePrefix("#")
     val cullFace: Directions?
-    
+
     val positions: Array<Vec2>
 
     init {
