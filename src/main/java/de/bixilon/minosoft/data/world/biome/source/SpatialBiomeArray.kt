e5e71bf1cb3ae1450feffe2cb28a3fdeeb1b74ad
Bixilon <bixilon@bixilon.de>
2021-04-09 01:02:10 +0200

    fix ArrayOutOufBoundsException when 3d biomes are supported in 19w36a+

--- src/main/java/de/bixilon/minosoft/data/world/biome/source/SpatialBiomeArray.kt
+++ src/main/java/de/bixilon/minosoft/data/world/biome/source/SpatialBiomeArray.kt
@@ -20,7 +20,7 @@ import glm_.vec3.Vec3i
 class SpatialBiomeArray(private val data: Array<Biome>) : BiomeSource {
 
     override fun getBiome(position: Vec3i): Biome {
-        val index = (MMath.clamp(position.y, 0, Y_BIT_MASK)) shl 2 * X_SECTION_COUNT + X_SECTION_COUNT or
+        val index = (MMath.clamp(position.y, 0, Y_BIT_MASK)) shl X_SECTION_COUNT + X_SECTION_COUNT or
                 ((position.z and X_BIT_MASK) shl X_SECTION_COUNT) or
                 (position.x and X_BIT_MASK)
 
