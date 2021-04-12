11ec3fcb62248c40abdd515060bfae95d1c7a3c6
Bixilon <bixilon@bixilon.de>
2021-04-12 21:21:51 +0200

    convert rest of entity classes to kotlin

--- src/main/java/de/bixilon/minosoft/data/entities/entities/monster/PatrollingMonster.kt
+++ src/main/java/de/bixilon/minosoft/data/entities/entities/monster/PatrollingMonster.kt
@@ -10,17 +10,11 @@
  *
  * This software is not affiliated with Mojang AB, the original developer of Minecraft.
  */
+package de.bixilon.minosoft.data.entities.entities.monster
 
-package de.bixilon.minosoft.data.entities.entities.monster;
+import de.bixilon.minosoft.data.entities.EntityRotation
+import de.bixilon.minosoft.data.mappings.entities.EntityType
+import de.bixilon.minosoft.protocol.network.connection.PlayConnection
+import glm_.vec3.Vec3
 
-import de.bixilon.minosoft.data.entities.EntityRotation;
-import de.bixilon.minosoft.data.mappings.entities.EntityType;
-import de.bixilon.minosoft.protocol.network.connection.PlayConnection;
-import glm_.vec3.Vec3;
-
-public abstract class PatrollingMonster extends Monster {
-
-    public PatrollingMonster(PlayConnection connection, EntityType entityType, Vec3 position, EntityRotation rotation) {
-        super(connection, entityType, position, rotation);
-    }
-}
+abstract class PatrollingMonster(connection: PlayConnection, entityType: EntityType, position: Vec3, rotation: EntityRotation) : Monster(connection, entityType, position, rotation)
