6d9fea3cc95eba789c4f3cad790690cad7be29b3
Bixilon <bixilon@bixilon.de>
2021-05-21 13:24:34 +0200

    support for 21w20a

--- src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.java
+++ src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.java
@@ -16,6 +16,7 @@ package de.bixilon.minosoft.protocol.protocol;
 @SuppressWarnings("unused")
 public class ProtocolVersions {
     public static final int
+            V_21W20A = 780,
             V_21W19A = 779,
             V_21W18A = 778,
             V_21W17A = 777,
