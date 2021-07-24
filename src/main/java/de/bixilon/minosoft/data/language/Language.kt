143d5e197e1321938ae6e53c21f8f22521dd1f0a
Bixilon <bixilon@bixilon.de>
2021-07-25 00:53:58 +0200

    language translator: fix bug in string splitting

--- src/main/java/de/bixilon/minosoft/data/language/Language.kt
+++ src/main/java/de/bixilon/minosoft/data/language/Language.kt
@@ -33,17 +33,17 @@ class Language(
 
         val arguments: MutableList<Any?> = mutableListOf()
         var splitPlaceholder: List<String> = listOf()
-        FORMATTER_ORDER_REGEX.find(placeholder)?.let {
-            if (it.groupValues.isEmpty()) {
+
+        // Bring arguments in correct oder
+        FORMATTER_ORDER_REGEX.findAll(placeholder).toList().let {
+            if (it.isEmpty()) {
                 // this is not the correct formatter
                 return@let
             }
             splitPlaceholder = placeholder.split(FORMATTER_ORDER_REGEX)
-            for ((index, part) in it.groupValues.withIndex()) {
-                if (index % 2 == 0) {
-                    continue
-                }
-                val dataIndex = part.toInt() - 1
+            for (matchResult in it) {
+                // 2 groups: Full, index. We don't care about the full value, just skip it
+                val dataIndex = matchResult.groupValues[1].toInt() - 1
                 if (dataIndex < 0 || dataIndex > data.size) {
                     arguments += null
                     continue
@@ -52,14 +52,15 @@ class Language(
             }
         }
 
-
-        placeholder.split(FORMATTER_SPLIT_REGEX).let {
-            if (splitPlaceholder.isEmpty()) {
+        // check if other splitter already did the job for us
+        if (splitPlaceholder.isEmpty()) {
+            placeholder.split(FORMATTER_SPLIT_REGEX).let {
                 splitPlaceholder = it
+                arguments.addAll(data.toList())
             }
-            arguments.addAll(data.toList())
         }
 
+        // create base component
         for ((index, part) in splitPlaceholder.withIndex()) {
             ret += ChatComponent.of(part, this, parent)
             if (index < data.size) {
