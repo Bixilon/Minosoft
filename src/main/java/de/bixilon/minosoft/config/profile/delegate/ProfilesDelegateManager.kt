b6d0696cd533065097a82a98c19c6599434eac68
Bixilon <bixilon@bixilon.de>
2022-01-05 02:05:22 +0100

    add missing copyrights

--- src/main/java/de/bixilon/minosoft/config/profile/delegate/ProfilesDelegateManager.kt
+++ src/main/java/de/bixilon/minosoft/config/profile/delegate/ProfilesDelegateManager.kt
@@ -1,3 +1,16 @@
+/*
+ * Minosoft
+ * Copyright (C) 2020-2022 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not affiliated with Mojang AB, the original developer of Minecraft.
+ */
+
 package de.bixilon.minosoft.config.profile.delegate
 
 import de.bixilon.kutil.cast.CastUtil.unsafeCast
@@ -14,7 +27,7 @@ object ProfilesDelegateManager {
     private val listeners: SynchronizedMap<String, SynchronizedMap<Profile?, MutableSet<Pair<WeakReference<Any>, ProfileDelegateWatcher<Any>>>>> = synchronizedMapOf()
 
     fun <T> register(reference: Any, listener: ProfileDelegateWatcher<T>) {
-        this.listeners.getOrPut(listener.fieldIdentifier) { synchronizedMapOf() }.getOrPut(listener.profile) { synchronizedSetOf() }.add(Pair(WeakReference(reference), listener.unsafeCast()))
+        this.listeners.synchronizedGetOrPut(listener.fieldIdentifier) { synchronizedMapOf() }.synchronizedGetOrPut(listener.profile) { synchronizedSetOf() }.add(Pair(WeakReference(reference), listener.unsafeCast()))
     }
 
     fun onChange(profile: Profile, fieldIdentifier: String, previous: Any?, value: Any?) {
