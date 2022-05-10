68a74b0fb3d5be6be4403c9745982ec61df44e5f
Bixilon <bixilon@bixilon.de>
2022-05-10 22:49:36 +0200

    fix crash when trying to place block above dimensions max height

--- src/main/java/de/bixilon/minosoft/data/world/Chunk.kt
+++ src/main/java/de/bixilon/minosoft/data/world/Chunk.kt
@@ -174,7 +174,7 @@ class Chunk(
     private fun getOrPut(sectionHeight: Int): ChunkSection? {
         val sections = sections ?: return null
         val sectionIndex = sectionHeight - lowestSection
-        if (sectionIndex < 0 || sectionIndex > sections.size) {
+        if (sectionIndex < 0 || sectionIndex >= sections.size) {
             return null
         }
 
