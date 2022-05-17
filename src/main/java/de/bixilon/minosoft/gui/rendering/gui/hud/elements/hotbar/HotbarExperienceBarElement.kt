899b838b2c747e4494db876b0c74d74213d9e71b
Bixilon <bixilon@bixilon.de>
2022-05-17 18:03:04 +0200

    hud: fix level display in hotbar

--- src/main/java/de/bixilon/minosoft/gui/rendering/gui/hud/elements/hotbar/HotbarExperienceBarElement.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/gui/hud/elements/hotbar/HotbarExperienceBarElement.kt
@@ -76,7 +76,7 @@ class HotbarExperienceBarElement(guiRenderer: GUIRenderer) : Element(guiRenderer
 
         if (!jumping) {
             val experienceCondition = guiRenderer.renderWindow.connection.player.experienceCondition
-            if (this.jumping != jumping || progress != experienceCondition.experienceBarProgress) {
+            if (this.jumping != jumping || progress != experienceCondition.experienceBarProgress || this.level != experienceCondition.level) {
                 this.progress = experienceCondition.experienceBarProgress
                 this.jumping = jumping
                 this.level = experienceCondition.level
