4ddddb31e5e8deb6dc2b2ed3741fda7948975634
Bixilon <bixilon@bixilon.de>
2022-06-04 16:46:45 +0200

    network: 1.19-rc2

--- src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.kt
+++ src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.kt
@@ -14,6 +14,7 @@ package de.bixilon.minosoft.protocol.protocol
 
 @Suppress("UNUSED")
 object ProtocolVersions {
+    const val V_1_19_RC2 = 847
     const val V_1_19_RC1 = 846
     const val V_1_19_PRE5 = 845
     const val V_1_19_PRE4 = 844
