Bixilon/Minosoft

commit e0810829432748e718bb050740c91e053850bd62

inline some functions, improve fluid rendering performance

src/main/java/de/bixilon/minosoft/gui/rendering/util/VecUtil.kt
@@ -118,14 +118,10 @@ object VecUtil {
         return this * cos + (axis cross this) * sin + axis * (axis dot this) * (1 - cos)
     }
 
-    val Int.inSectionHeight: Int
-        get() = if (this < 0) {
-            ((ProtocolDefinition.SECTION_HEIGHT_Y + (this % ProtocolDefinition.SECTION_HEIGHT_Y))) % ProtocolDefinition.SECTION_HEIGHT_Y
-        } else {
-            this % ProtocolDefinition.SECTION_HEIGHT_Y
-        }
+    inline val Int.inSectionHeight: Int
+        get() = this and 4
 
-    val Int.sectionHeight: Int
+    inline val Int.sectionHeight: Int
         get() = this shr 4
 
     val Vec3i.entityPosition: Vec3d
@@ -153,37 +149,37 @@ object VecUtil {
         ) // ToDo: Confirm
     }
 
-    infix operator fun Vec3i.plus(vec3: Vec3i?): Vec3i {
+    inline infix operator fun Vec3i.plus(vec3: Vec3i?): Vec3i {
         if (vec3 == null) {
             return this
         }
         return Vec3i((x + vec3.x), (y + vec3.y), (z + vec3.z))
     }
 
-    infix operator fun Vec3i.plus(vec2: Vec2i?): Vec3i {
+    inline infix operator fun Vec3i.plus(vec2: Vec2i?): Vec3i {
         if (vec2 == null) {
             return this
         }
         return Vec3i((x + vec2.x), y, (z + vec2.y))
     }
 
-    infix operator fun Vec3i.plus(direction: Directions?): Vec3i {
+    inline infix operator fun Vec3i.plus(direction: Directions?): Vec3i {
         return this + direction?.vector
     }
 
-    infix operator fun Vec3i.plusAssign(direction: Directions?) {
+    inline infix operator fun Vec3i.plusAssign(direction: Directions?) {
         this += direction?.vector ?: return
     }
 
-    infix operator fun Vec3i.plus(input: Vec3): Vec3 {
+    inline infix operator fun Vec3i.plus(input: Vec3): Vec3 {
         return Vec3(input.x + x, input.y + y, input.z + z)
     }
 
-    infix operator fun Vec2i.plus(vec3: Vec3i): Vec2i {
+    inline infix operator fun Vec2i.plus(vec3: Vec3i): Vec2i {
         return Vec2i(x + vec3.x, y + vec3.z)
     }
 
-    infix operator fun Vec2i.plus(direction: Directions): Vec2i {
+    inline infix operator fun Vec2i.plus(direction: Directions): Vec2i {
         return this + direction.vector
     }
 

src/main/java/de/bixilon/minosoft/gui/rendering/world/preparer/cull/FluidCullSectionPreparer.kt
@@ -180,49 +180,50 @@ class FluidCullSectionPreparer(
                         if (skip[Directions.SIDE_OFFSET + direction]) {
                             continue
                         }
-                        var faceX = 0.0f
-                        var faceXEnd = 0.0f
-                        var faceZ = 0.0f
-                        var faceZEnd = 0.0f
-                        var v1 = 0.0f
-                        var v2 = 0.0f
+                        var faceX = position.x.toFloat()
+                        var faceXEnd = faceX
+                        var faceZ = position.z.toFloat()
+                        var faceZEnd = faceZ
+                        var v1 = position.y.toFloat()
+                        var v2 = v1
 
                         when (direction) {
                             0 -> {
-                                faceXEnd = 1.0f
-                                v1 = cornerHeights[0]
-                                v2 = cornerHeights[1]
+                                faceXEnd += 1.0f
+                                v1 += cornerHeights[0]
+                                v2 += cornerHeights[1]
                             }
 
                             1 -> {
-                                faceX = 1.0f
-                                faceZ = 1.0f
-                                faceZEnd = 1.0f
-                                v1 = cornerHeights[2]
-                                v2 = cornerHeights[3]
+                                faceX += 1.0f
+                                faceZ = +1.0f
+                                faceZEnd += 1.0f
+                                v1 += cornerHeights[2]
+                                v2 += cornerHeights[3]
                             }
 
                             2 -> {
-                                faceZ = 1.0f
-                                v1 = cornerHeights[3]
-                                v2 = cornerHeights[0]
+                                faceZ += 1.0f
+                                v1 += cornerHeights[3]
+                                v2 += cornerHeights[0]
                             }
 
                             3 -> {
-                                faceX = 1.0f
-                                faceXEnd = 1.0f
-                                faceZEnd = 1.0f
-                                v1 = cornerHeights[1]
-                                v2 = cornerHeights[2]
+                                faceX += 1.0f
+                                faceXEnd += 1.0f
+                                faceZEnd += 1.0f
+                                v1 += cornerHeights[1]
+                                v2 += cornerHeights[2]
                             }
                         }
                         // ToDo: Prevent face fighting with transparent neighbours
 
+
                         val positions = arrayOf(
-                            Vec3(position.x + faceX, position.y + v1, position.z + faceZ),
-                            Vec3(position.x + faceX, position.y, position.z + faceZ),
-                            Vec3(position.x + faceXEnd, position.y, position.z + faceZEnd),
-                            Vec3(position.x + faceXEnd, position.y + v2, position.z + faceZEnd),
+                            Vec3(faceX, v1, faceZ),
+                            Vec3(faceX, position.y, faceZ),
+                            Vec3(faceXEnd, position.y, faceZEnd),
+                            Vec3(faceXEnd, v2, faceZEnd),
                         )
                         val texturePositions = arrayOf(
                             TEXTURE_1,

commit fcd3735426d6b323797bd6a471c55e281a3faa28

fix inSectionHeight calculation
Got broken in e0810829432748e718bb050740c91e053850bd62

src/main/java/de/bixilon/minosoft/gui/rendering/util/VecUtil.kt
@@ -119,7 +119,7 @@ object VecUtil {
     }
 
     inline val Int.inSectionHeight: Int
-        get() = this and 4
+        get() = this and 0x0F
 
     inline val Int.sectionHeight: Int
         get() = this shr 4