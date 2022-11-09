4b9110ccfc1b8138eae1b89624851a75af2222a9
Bixilon <bixilon@bixilon.de>
2022-11-09 22:45:17 +0100

    dimension properties: remove unused parameters

--- src/main/java/de/bixilon/minosoft/data/registries/dimension/DimensionProperties.kt
+++ src/main/java/de/bixilon/minosoft/data/registries/dimension/DimensionProperties.kt
@@ -18,7 +18,6 @@ import de.bixilon.kutil.math.interpolation.FloatInterpolation.interpolateLinear
 import de.bixilon.kutil.primitive.BooleanUtil.toBoolean
 import de.bixilon.kutil.primitive.FloatUtil.toFloat
 import de.bixilon.kutil.primitive.IntUtil.toInt
-import de.bixilon.minosoft.data.registries.ResourceLocation
 import de.bixilon.minosoft.gui.rendering.sky.properties.DefaultSkyProperties
 import de.bixilon.minosoft.gui.rendering.sky.properties.OverworldSkyProperties
 import de.bixilon.minosoft.gui.rendering.sky.properties.SkyProperties
@@ -27,19 +26,18 @@ import de.bixilon.minosoft.util.KUtil.toResourceLocation
 import de.bixilon.minosoft.util.nbt.tag.NBTUtil.get
 
 data class DimensionProperties(
-    val piglinSafe: Boolean = false,
-    val natural: Boolean = true,
+    //   val piglinSafe: Boolean = false,
+    //   val natural: Boolean = true,
     val ambientLight: Float = 0.0f,
-    val infiniBurn: ResourceLocation = ResourceLocation("infiniburn_overworld"),
-    val respawnAnchorWorks: Boolean = false,
+    //   val respawnAnchorWorks: Boolean = false,
     val hasSkyLight: Boolean = true,
-    val bedWorks: Boolean = true,
+    //   val bedWorks: Boolean = true,
     val skyProperties: SkyProperties = OverworldSkyProperties,
-    val hasRaids: Boolean = true,
+    //   val hasRaids: Boolean = true,
     val logicalHeight: Int = DEFAULT_HEIGHT,
-    val coordinateScale: Double = 0.0,
+    //   val coordinateScale: Double = 0.0,
     val minY: Int = 0,
-    val hasCeiling: Boolean = false,
+    //   val hasCeiling: Boolean = false,
     val ultraWarm: Boolean = false,
     val dataHeight: Int = DEFAULT_HEIGHT,
     val supports3DBiomes: Boolean = true,
@@ -60,12 +58,9 @@ data class DimensionProperties(
         check(minSection in ProtocolDefinition.CHUNK_MIN_SECTION..ProtocolDefinition.CHUNK_MAX_SECTION) { "Minimum section out of bounds: $minSection" }
         check(maxSection in ProtocolDefinition.CHUNK_MIN_SECTION..ProtocolDefinition.CHUNK_MAX_SECTION) { "Maximum section out of bounds: $minSection" }
 
-        val ambientLight = 0.0f // ToDo: 0.1 in nether
-
         for (i in lightLevels.indices) {
-            val asFloat = i / 15.0f
-
-            lightLevels[i] = interpolateLinear(ambientLight, asFloat / (4.0f - 3.0f * asFloat), 1.0f)
+            val fraction = i / 15.0f
+            lightLevels[i] = interpolateLinear(ambientLight, fraction / (4.0f - 3.0f * fraction), 1.0f)
         }
     }
 
@@ -76,19 +71,19 @@ data class DimensionProperties(
 
         fun deserialize(data: Map<String, Any>): DimensionProperties {
             return DimensionProperties(
-                piglinSafe = data["piglin_safe"]?.toBoolean() ?: false,
-                natural = data["natural"]?.toBoolean() ?: false,
+                //piglinSafe = data["piglin_safe"]?.toBoolean() ?: false,
+                //natural = data["natural"]?.toBoolean() ?: false,
                 ambientLight = data["ambient_light"]?.toFloat() ?: 0.0f,
-                infiniBurn = ResourceLocation(data["infiniburn"].nullCast<String>() ?: "infiniburn_overworld"),
-                respawnAnchorWorks = data["respawn_anchor_works"]?.toBoolean() ?: false,
+                //infiniBurn = ResourceLocation(data["infiniburn"].nullCast<String>() ?: "infiniburn_overworld"),
+                //respawnAnchorWorks = data["respawn_anchor_works"]?.toBoolean() ?: false,
                 hasSkyLight = data["has_skylight", "has_sky_light"]?.toBoolean() ?: false,
-                bedWorks = data["bed_works"]?.toBoolean() ?: false,
+                //bedWorks = data["bed_works"]?.toBoolean() ?: false,
                 skyProperties = data["effects"].nullCast<String>()?.let { DefaultSkyProperties[it.toResourceLocation()] } ?: OverworldSkyProperties,
-                hasRaids = data["has_raids"]?.toBoolean() ?: false,
+                //hasRaids = data["has_raids"]?.toBoolean() ?: false,
                 logicalHeight = data["logical_height"]?.toInt() ?: DEFAULT_MAX_Y,
-                coordinateScale = data["coordinate_scale"].nullCast() ?: 0.0,
+                //coordinateScale = data["coordinate_scale"].nullCast() ?: 0.0,
                 minY = data["min_y"]?.toInt() ?: 0,
-                hasCeiling = data["has_ceiling"]?.toBoolean() ?: false,
+                //hasCeiling = data["has_ceiling"]?.toBoolean() ?: false,
                 ultraWarm = data["ultrawarm"]?.toBoolean() ?: false,
                 dataHeight = data["height"]?.toInt() ?: DEFAULT_MAX_Y,
                 supports3DBiomes = data["supports_3d_biomes"]?.toBoolean() ?: true,
