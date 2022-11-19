51dffe042b4a0a28ea25f3a3104ec9173720e616
Bixilon <bixilon@bixilon.de>
2022-11-19 16:41:19 +0100

    network: 22w46a

--- src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.kt
+++ src/main/java/de/bixilon/minosoft/protocol/protocol/ProtocolVersions.kt
@@ -14,6 +14,7 @@ package de.bixilon.minosoft.protocol.protocol
 
 @Suppress("UNUSED")
 object ProtocolVersions {
+    const val V_22W46A = 867
     const val V_22W45A = 866
     const val V_22W44A = 865
     const val V_22W43A = 864
