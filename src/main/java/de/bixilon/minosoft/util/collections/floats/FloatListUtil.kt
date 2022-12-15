e8a185d10d8d52bb0a66dd0eca033ef95e5e8774
Bixilon <bixilon@bixilon.de>
2022-12-15 19:17:50 +0100

    temporary disable fragmented list (bug fix inc)

--- src/main/java/de/bixilon/minosoft/util/collections/floats/FloatListUtil.kt
+++ src/main/java/de/bixilon/minosoft/util/collections/floats/FloatListUtil.kt
@@ -19,7 +19,7 @@ import org.lwjgl.system.MemoryUtil.memAllocFloat
 import java.nio.FloatBuffer
 
 object FloatListUtil {
-    const val PREFER_FRAGMENTED = true
+    const val PREFER_FRAGMENTED = false
 
     val FLOAT_PUT_METHOD = ExceptionUtil.catchAll { FloatBuffer::class.java.getMethod("put", Int::class.java, FloatBuffer::class.java, Int::class.java, Int::class.java) }
     const val DEFAULT_INITIAL_SIZE = 1000
