f3b11e8ef1d9f5f112db42fc2b90f54d4f0e62ef
Bixilon <bixilon@bixilon.de>
2023-01-26 21:19:59 +0100

    glfw: only scale screen positions on macOS #29

--- src/main/java/de/bixilon/minosoft/gui/rendering/system/window/BaseWindow.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/system/window/BaseWindow.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2023 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -25,6 +25,8 @@ import org.lwjgl.BufferUtils
 import java.nio.ByteBuffer
 
 interface BaseWindow {
+    val systemScale: Vec2
+
     var size: Vec2i
     val sizef: Vec2
         get() = Vec2(size)
