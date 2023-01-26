6fc6ad5a5b214accbff14770542a8d00a74a8e4f
Bixilon <bixilon@bixilon.de>
2023-01-26 21:45:56 +0100

    chat component renderer: don't emit overflow flag when message is empty
    This fixes some bugs on weird servers that a lot of messages in the chat are hidden

--- src/main/java/de/bixilon/minosoft/gui/rendering/font/renderer/ChatComponentRenderer.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/font/renderer/ChatComponentRenderer.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2023 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -47,7 +47,7 @@ interface ChatComponentRenderer<T : ChatComponent> {
             return when (text) {
                 is BaseComponent -> BaseComponentRenderer.render(initialOffset, offset, size, element, context, consumer, options, renderInfo, text)
                 is TextComponent -> TextComponentRenderer.render(initialOffset, offset, size, element, context, consumer, options, renderInfo, text)
-                is EmptyComponent -> return true
+                is EmptyComponent -> return false
                 else -> TODO("Don't know how to render ${text::class.java}")
             }
         }
