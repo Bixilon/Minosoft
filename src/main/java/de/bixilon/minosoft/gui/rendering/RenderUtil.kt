84c47c45a27e4df5c236b92ed183af7471f0ad37
Bixilon <bixilon@bixilon.de>
2023-02-01 21:34:15 +0100

    fix gui freeze if poll is busy
    It actually should crash and not freeze :)

--- src/main/java/de/bixilon/minosoft/gui/rendering/RenderUtil.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/RenderUtil.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2023 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -31,7 +31,7 @@ object RenderUtil {
     }
 
     fun RenderContext.runAsync(runnable: () -> Unit) {
-        DefaultThreadPool += ThreadPoolRunnable(ThreadPool.HIGHER) {
+        DefaultThreadPool += ThreadPoolRunnable(ThreadPool.HIGHER, forcePool = false) {
             try {
                 runnable()
             } catch (error: Throwable) {
