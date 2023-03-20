f786af9178d86d2e940d6b82f09efbf946296854
Bixilon <bixilon@bixilon.de>
2023-03-20 23:22:05 +0100

    network: fix 1.8 PositionRotationS2CP reading

--- src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/entity/move/PositionRotationS2CP.kt
+++ src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/entity/move/PositionRotationS2CP.kt
@@ -27,7 +27,7 @@ import de.bixilon.minosoft.util.logging.LogLevels
 import de.bixilon.minosoft.util.logging.LogMessageType
 
 class PositionRotationS2CP(buffer: PlayInByteBuffer) : PlayS2CPacket {
-    val position: Vec3d = buffer.readVec3d()
+    val position: Vec3d = Vec3d(buffer.readDoubleArray(3))
     val rotation: EntityRotation
     var onGround = false
     private var flags: Int = 0
