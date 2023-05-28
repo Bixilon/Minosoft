17c48d9ff7d44156b48974e4f181b78c6d57b9f0
Bixilon <bixilon@bixilon.de>
2023-05-28 14:33:35 +0200

    remove damage shaking
    Need to be reimplemented

--- src/main/java/de/bixilon/minosoft/gui/rendering/camera/shaking/CameraShaking.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/camera/shaking/CameraShaking.kt
@@ -28,26 +28,40 @@ class CameraShaking(
     private val profile: ShakingC,
 ) {
     private var rotation = 0.0f
-    private var strength = 0.0f
     private val speed = FloatAverage(10 * ProtocolDefinition.TICK_TIME * 1_000_000L, 0.0f)
     private val physics = camera.context.connection.camera.entity.physics
 
     val isEmpty: Boolean get() = rotation == 0.0f
 
     fun update(): Boolean {
-        if (!this.physics.onGround) {
-            this.speed += 0.0f
-            this.rotation = 0.0f
-            return false
+        var bobbing = 0.0f
+        if (profile.walking) {
+            bobbing = updateBobbing()
         }
-        val time = millis()
-        this.rotation = bobbing(time, 10.0f, this.strength)
-        this.strength = STRENGTH * profile.amplifier
+        var damage = 0.0f
+        if (profile.damage) {
+            damage = updateDamage()
+        }
+
+        val rotation = bobbing + damage
+
+        if (rotation == this.rotation) return false
+        this.rotation = rotation
         return true
     }
 
-    private fun bobbing(time: Long, frequency: Float, intensity: Float): Float {
+    private fun updateBobbing(): Float {
+        if (!this.physics.onGround) {
+            this.speed += 0.0f
+            return 0.0f
+        }
+        val time = millis()
         this.speed += this.physics.velocity.xz.length2().toFloat() // velocity affects how quick it goes
+        val speed = this.speed.avg
+        return bobbing(time, speed, FREQUENCY * profile.speed, STRENGTH * profile.intensity)
+    }
+
+    private fun bobbing(time: Long, speed: Float, frequency: Float, intensity: Float): Float {
         val seconds = time / 1000.0
 
         val sin = sin(seconds * frequency).toFloat()
@@ -55,9 +69,13 @@ class CameraShaking(
         return (sin * speed * intensity) / MINIMUM_SPEED
     }
 
+    private fun updateDamage(): Float {
+        //TODO
+        return 0.0f
+    }
+
     fun onDamage() {
-        //TODO: verify this properly, frequency may need to be changed as well
-        this.strength += 1.0f
+        //TODO
     }
 
     fun transform(): Mat4? {
@@ -68,6 +86,7 @@ class CameraShaking(
 
     companion object {
         private const val STRENGTH = 0.001f
+        private const val FREQUENCY = 10.0f
         private const val MINIMUM_SPEED = 0.1f
     }
 }
