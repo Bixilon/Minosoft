78533acfad8679e46954bfe930dde51499bdf8c7
Moritz Zwerger <bixilon@bixilon.de>
2023-11-04 18:05:25 +0100

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,10 @@ import java.nio.charset.StandardCharsets
 
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     `jvm-test-suite`
     application
-    id("org.ajoberstar.grgit.service") version "5.2.0"
+    id("org.ajoberstar.grgit.service") version "5.2.1"
     id("com.github.ben-manes.versions") version "0.49.0"
 }
 
@@ -152,7 +152,7 @@ testing {
             dependencies {
                 implementation(project())
                 implementation("de.bixilon:kutil:$kutilVersion")
-                implementation("org.jetbrains.kotlin:kotlin-test:1.9.10")
+                implementation("org.jetbrains.kotlin:kotlin-test:1.9.20")
             }
 
             targets {
@@ -333,13 +333,13 @@ fun DependencyHandler.lwjgl(name: String? = null) {
 
 dependencies {
     implementation("org.slf4j", "slf4j-api", "2.0.9")
-    implementation("com.google.guava", "guava", "32.1.2-jre")
+    implementation("com.google.guava", "guava", "32.1.3-jre")
     implementation("dnsjava", "dnsjava", "3.5.2")
     implementation("net.sourceforge.argparse4j", "argparse4j", "0.9.0")
-    implementation("org.jline", "jline", "3.23.0")
+    implementation("org.jline", "jline", "3.24.1")
     implementation("org.l33tlabs.twl", "pngdecoder", "1.0")
-    implementation("com.github.oshi", "oshi-core", "6.4.6")
-    implementation("com.github.luben", "zstd-jni", "1.5.5-6", classifier = zstdNatives)
+    implementation("com.github.oshi", "oshi-core", "6.4.7")
+    implementation("com.github.luben", "zstd-jni", "1.5.5-10", classifier = zstdNatives)
     implementation("org.apache.commons", "commons-lang3", "3.13.0")
     implementation("org.kamranzafar", "jtar", "2.3")
     implementation("org.reflections", "reflections", "0.10.2")
@@ -379,7 +379,7 @@ dependencies {
     lwjgl("stb")
 
     // kotlin
-    implementation(kotlin("reflect", "1.9.10"))
+    implementation(kotlin("reflect", "1.9.20"))
 
 
     // platform specific
