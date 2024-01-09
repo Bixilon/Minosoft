bd4e7f114ff01abf97bd80a954f1ff8dc116df2c
Moritz Zwerger <bixilon@bixilon.de>
2024-01-09 22:37:46 +0100

    fix item durability in <1.13

--- src/main/java/de/bixilon/minosoft/data/container/ItemStackUtil.kt
+++ src/main/java/de/bixilon/minosoft/data/container/ItemStackUtil.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -24,7 +24,7 @@ object ItemStackUtil {
 
     fun of(item: Item, connection: PlayConnection, count: Int, meta: Int, nbt: MutableMap<String, Any>): ItemStack {
         if (connection.version.flattened) {
-            return of(item, count, connection, durability = meta, nbt = nbt)
+            return of(item, count, connection, nbt = nbt)
         }
         val stack = of(item, count, connection, nbt = nbt)
 
--- src/main/java/de/bixilon/minosoft/data/registries/item/items/DurableItem.kt
+++ src/main/java/de/bixilon/minosoft/data/registries/item/items/DurableItem.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -20,6 +20,6 @@ interface DurableItem : ItemWithMeta {
     val maxDurability: Int
 
     override fun setMeta(stack: ItemStack, meta: Int) {
-        stack.durability.durability = meta
+        stack.durability.durability = maxDurability - meta // in <1.13 its damage not durability
     }
 }
