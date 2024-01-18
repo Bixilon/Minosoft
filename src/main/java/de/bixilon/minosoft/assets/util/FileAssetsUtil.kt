abab3e83b70256784761eb8f4c095f84a9557908
Moritz Zwerger <bixilon@bixilon.de>
2024-01-18 13:39:40 +0100

    fix build

--- src/main/java/de/bixilon/minosoft/assets/util/FileAssetsUtil.kt
+++ src/main/java/de/bixilon/minosoft/assets/util/FileAssetsUtil.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -18,9 +18,9 @@ import com.github.luben.zstd.ZstdOutputStream
 import de.bixilon.kutil.array.ByteArrayUtil.toHex
 import de.bixilon.kutil.file.FileUtil.createParent
 import de.bixilon.minosoft.assets.AssetsManager
-import de.bixilon.minosoft.assets.util.FileUtil.copy
 import de.bixilon.minosoft.assets.util.HashTypes.Companion.hashType
 import de.bixilon.minosoft.data.registries.identified.ResourceLocation
+import de.bixilon.minosoft.util.KUtil.copy
 import de.bixilon.minosoft.util.logging.Log
 import de.bixilon.minosoft.util.logging.LogLevels
 import de.bixilon.minosoft.util.logging.LogMessageType
