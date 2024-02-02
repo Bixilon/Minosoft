fb1f460d5d70098eb97163346f5a224de5b639d4
Moritz Zwerger <bixilon@bixilon.de>
2024-02-02 20:40:51 +0100

    command reader: Use fast exception
    The stacktrace is mostly never read, just the message is print. This makes the cli output way cleaner and improves performance.

--- src/main/java/de/bixilon/minosoft/commands/errors/ReaderError.kt
+++ src/main/java/de/bixilon/minosoft/commands/errors/ReaderError.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -13,12 +13,13 @@
 
 package de.bixilon.minosoft.commands.errors
 
+import de.bixilon.kutil.exception.FastException
 import de.bixilon.minosoft.commands.util.StringReader
 
 abstract class ReaderError(
     val reader: StringReader,
     val start: Int,
     val end: Int,
-) : Exception() {
+) : FastException() {
     override val message: String = "${this::class.simpleName} at $start-$end: ${reader.string} (at ${reader.string.substring(start, end)})"
 }
