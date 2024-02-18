d88a08014a141748c7c489c69741fccef086992d
Moritz Zwerger <bixilon@bixilon.de>
2024-02-18 21:47:19 +0100

    commands: don't throw exception if no nodes are submitted
    Hides an exception https://github.com/Bixilon/Minosoft/issues/30

--- src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/chat/CommandsS2CP.kt
+++ src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/chat/CommandsS2CP.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -34,7 +34,7 @@ import de.bixilon.minosoft.util.logging.LogMessageType
 
 class CommandsS2CP(buffer: PlayInByteBuffer) : PlayS2CPacket {
     val nodes = buffer.readArray { buffer.readCommandNode() }.build()
-    val rootNode = nodes[buffer.readVarInt()].nullCast<ConnectionNode>()
+    val rootNode = buffer.readVarInt().let { if (nodes.isEmpty()) null else nodes[it] }.nullCast<ConnectionNode?>()
 
 
     override fun handle(connection: PlayConnection) {
