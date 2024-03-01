8829385e523779b74fae3b1018b7522abe0c4a01
Moritz Zwerger <bixilon@bixilon.de>
2024-03-01 16:05:36 +0100

    fix cloud raster reading

--- src/main/java/de/bixilon/minosoft/gui/rendering/sky/clouds/CloudMatrix.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/sky/clouds/CloudMatrix.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -37,10 +37,9 @@ class CloudMatrix {
             } else {
                 val y = i / CLOUD_MATRIX_SIZE
                 val x = i % CLOUD_MATRIX_SIZE
-                matrix[i] = buffer.getRGBA(x, y) ushr 24 == 0xFF
+                matrix[i] = buffer.getA(x, y) == 0xFF
             }
         }
-
     }
 
 
--- src/main/java/de/bixilon/minosoft/gui/rendering/sky/clouds/CloudRenderer.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/sky/clouds/CloudRenderer.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -165,8 +165,8 @@ class CloudRenderer(
         val over = (baseHeight - y)
         if (over > 0.0f) {
             yOffset = -(over / 15.0f).pow(2)
-            if (yOffset < -100.0f) {
-                yOffset = -100.0f
+            if (yOffset < -80.0f) {
+                yOffset = -80.0f
             }
         }
         shader.yOffset = yOffset
