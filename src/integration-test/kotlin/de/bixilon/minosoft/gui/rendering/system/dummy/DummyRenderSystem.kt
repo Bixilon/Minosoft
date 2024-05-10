472bad4cfa11a627efaf616287b4f36e2a036fe4
Moritz Zwerger <bixilon@bixilon.de>
2024-05-10 22:42:32 +0200

    fix test building

--- src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/system/dummy/DummyRenderSystem.kt
+++ src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/system/dummy/DummyRenderSystem.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -105,7 +105,7 @@ class DummyRenderSystem(
         return DummyFloatUniformBuffer(data)
     }
 
-    override fun createFramebuffer(color: Boolean, depth: Boolean): Framebuffer {
+    override fun createFramebuffer(color: Boolean, depth: Boolean, scale: Float): Framebuffer {
         return DummyFramebuffer()
     }
 
