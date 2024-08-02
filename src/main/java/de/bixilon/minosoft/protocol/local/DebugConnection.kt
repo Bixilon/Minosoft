e1f5cca87ee5e7058f05f0b416c6c55f8962ab90
Moritz Zwerger <bixilon@bixilon.de>
2024-08-02 15:10:17 +0200

    create some sort of debug world

--- src/main/java/de/bixilon/minosoft/protocol/local/DebugConnection.kt
+++ src/main/java/de/bixilon/minosoft/protocol/local/DebugConnection.kt
@@ -17,12 +17,12 @@ import de.bixilon.kotlinglm.vec3.Vec3d
 import de.bixilon.kutil.observer.DataObserver.Companion.observed
 import de.bixilon.minosoft.data.abilities.Gamemodes
 import de.bixilon.minosoft.data.entities.entities.player.local.Abilities
-import de.bixilon.minosoft.data.registries.blocks.types.building.stone.StoneBlock
 import de.bixilon.minosoft.data.registries.dimension.DimensionProperties
 import de.bixilon.minosoft.data.registries.identified.Namespaces.minecraft
 import de.bixilon.minosoft.data.world.World
 import de.bixilon.minosoft.data.world.biome.source.DummyBiomeSource
 import de.bixilon.minosoft.data.world.positions.ChunkPosition
+import de.bixilon.minosoft.modding.event.events.DimensionChangeEvent
 import de.bixilon.minosoft.protocol.ServerConnection
 import de.bixilon.minosoft.protocol.network.session.Session
 import de.bixilon.minosoft.protocol.network.session.play.PlaySession
@@ -31,6 +31,7 @@ import de.bixilon.minosoft.protocol.packets.c2s.C2SPacket
 import de.bixilon.minosoft.util.logging.Log
 import de.bixilon.minosoft.util.logging.LogLevels
 import de.bixilon.minosoft.util.logging.LogMessageType
+import kotlin.math.sqrt
 
 class DebugConnection : ServerConnection {
     override val identifier = "dbg"
@@ -38,14 +39,30 @@ class DebugConnection : ServerConnection {
     private var detached = false
     private lateinit var session: PlaySession
 
+
     private fun World.createChunks(position: ChunkPosition, radius: Int) {
         val biome = session.registries.biome[minecraft("plains")]
-        val stone = session.registries.block[StoneBlock.Block]?.states?.default
         for (x in position.x - radius until position.x + radius) {
             for (z in position.y - radius until position.y + radius) {
                 val chunk = chunks.create(ChunkPosition(x, z))
                 chunk.biomeSource = DummyBiomeSource(biome)
-                chunk[0, 0, 0] = stone
+                chunk[0, 0, 0] = null // create lowest section
+            }
+        }
+    }
+
+    private fun fillDebug() {
+        val total = session.registries.blockState.size
+        val width = sqrt(total.toFloat()).toInt() + 1
+        var count = 0
+
+        for (block in session.registries.block) {
+            for (state in block.states) {
+                val x = (count / width - (width / 2)) * 2
+                val z = (count % width - (width / 2)) * 2
+                val chunk = session.world.chunks.create(ChunkPosition(x shr 4, z shr 4))
+                chunk[x and 0x0F, 8, z and 0x0F] = state
+                count++
             }
         }
     }
@@ -57,12 +74,28 @@ class DebugConnection : ServerConnection {
         this.session = session
 
 
+        session.util.resetWorld()
+        session.util.prepareSpawn()
+
+
         session.world.dimension = DimensionProperties()
         session.player.additional.gamemode = Gamemodes.CREATIVE
+
+
+        session.world.entities.clear(session, local = true)
+        session.world.entities.add(1, null, session.player)
+
         session.player.abilities = Abilities(false, true, true)
-        session.player.physics.forceTeleport(Vec3d(0, 100, 0))
         session.world.createChunks(ChunkPosition(0, 0), 5)
+
+        session.events.fire(DimensionChangeEvent(session))
+        session.state = PlaySessionStates.SPAWNING
+        session.player.physics.forceTeleport(Vec3d(0, 20, 0))
         session.state = PlaySessionStates.PLAYING
+
+
+
+        fillDebug()
     }
 
     override fun disconnect() {
