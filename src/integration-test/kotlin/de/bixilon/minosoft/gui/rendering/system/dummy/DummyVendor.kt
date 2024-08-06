c473b43154981edf1819f12f0cc368d2a16fa31d
Moritz Zwerger <bixilon@bixilon.de>
2024-08-06 23:12:29 +0200

    DummyVendor::toString

--- src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/system/dummy/DummyVendor.kt
+++ src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/system/dummy/DummyVendor.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2024 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -20,4 +20,6 @@ object DummyVendor : GPUVendor {
     override val shaderDefine: String = "__DUMMY"
 
     override val hacks = DriverHacks.set()
+
+    override fun toString() = "dummy"
 }
