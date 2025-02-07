f22935b5f109f73309bdd96f6c9c2027e432f679
Moritz Zwerger <bixilon@bixilon.de>
2025-02-07 22:14:47 +0100

    arm render: enable face culling
    Looks pretty shitty otherwise

--- src/main/java/de/bixilon/minosoft/gui/rendering/camera/arm/ArmRenderer.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/camera/arm/ArmRenderer.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2024 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -90,7 +90,7 @@ class ArmRenderer(override val context: RenderContext) : Renderer, Drawable {
 
         context.system.clear(IntegratedBufferTypes.DEPTH_BUFFER)
 
-        context.system.reset(faceCulling = false, depthTest = true, blending = true, depthMask = true)
+        context.system.reset(faceCulling = true, depthTest = true, blending = true, depthMask = true)
 
         shader.use()
         shader.skinParts = renderer.model?.skinParts ?: 0xFF
