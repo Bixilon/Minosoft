128ced029d961c714c485b3570b522eee3c3debd
Moritz Zwerger <bixilon@bixilon.de>
2025-02-08 19:32:39 +0100

    linux: force x11
    Wayland simply crashes and does not work at all.

--- src/main/java/de/bixilon/minosoft/gui/rendering/system/window/glfw/GLFWWindow.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/system/window/glfw/GLFWWindow.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2024 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -447,6 +447,9 @@ class GLFWWindow(
             }
             DefaultThreadPool += {
                 GLFWErrorCallback.createPrint(Log.FATAL_PRINT_STREAM).set()
+                if (PlatformInfo.OS == OSTypes.LINUX) {
+                    glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11)
+                }
                 check(glfwInit()) { "Unable to initialize GLFW" }
                 initLatch.dec()
             }
