df23b7ff70b74d166aaf5f827eba16092bbd3527
Moritz Zwerger <bixilon@bixilon.de>
2025-03-25 20:26:09 +0100

    skeletal transform packing: don't copy every single float from mat4
    Its batched now, the byte buffer can optimize this call.

--- src/main/java/de/bixilon/minosoft/gui/rendering/skeletal/instance/TransformInstance.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/skeletal/instance/TransformInstance.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -48,9 +48,8 @@ class TransformInstance(
         val array = value.array
 
         val offset = this.id * Mat4.length
-        for (index in 0 until Mat4.length) {
-            buffer.put(offset + index, array[index])
-        }
+        buffer.position(offset)
+        buffer.put(array, 0, Mat4.length)
 
         for (child in this.array) {
             child.pack(buffer)
