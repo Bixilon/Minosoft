5b4c5157d81563a9230e4461819a410cd1d46c9e
Moritz Zwerger <bixilon@bixilon.de>
2025-03-26 23:37:19 +0100

    hud: reduce text memory allocations

--- src/main/java/de/bixilon/minosoft/gui/rendering/gui/elements/layout/grid/GridCell.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/gui/elements/layout/grid/GridCell.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -40,19 +40,16 @@ class GridCell(
         _parent = parent
     }
 
-    override val maxSize: Vec2
-        get() {
-            val maxSize = Vec2(super.maxSize)
+    override fun applyMaxSize(max: Vec2) {
+        super.applyMaxSize(max)
 
-            if (columnConstraint.width < maxSize.x) {
-                maxSize.x = columnConstraint.width
-            }
-            if (rowConstraint.height < maxSize.y) {
-                maxSize.y = rowConstraint.height
-            }
-
-            return maxSize
+        if (columnConstraint.width < max.x) {
+            max.x = columnConstraint.width
         }
+        if (rowConstraint.height < max.y) {
+            max.y = rowConstraint.height
+        }
+    }
 
     init {
         child.parent = this
