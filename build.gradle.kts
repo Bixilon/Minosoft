d1086cf67e2fc7c0ce0e34d33da0af7fd154af8f
Moritz Zwerger <bixilon@bixilon.de>
2025-09-29 13:52:19 +0200

    gradle: don't get from maven local
    This is sometimes fucked up.

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ val architecture = properties["architecture"]?.let { Architectures[it] } ?: Plat
 logger.info("Building for ${os.name.lowercase()}, ${architecture.name.lowercase()}")
 
 repositories {
-    mavenLocal()
+    // mavenLocal()
     mavenCentral()
     maven(url = "https://s01.oss.sonatype.org/content/repositories/releases/")
 }
