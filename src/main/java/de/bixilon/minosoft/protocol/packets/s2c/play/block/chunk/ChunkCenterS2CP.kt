8791b1efac42585c999546787103877d8de53e7a
Moritz Zwerger <bixilon@bixilon.de>
2025-10-03 12:38:16 +0200

    network: don't log chunk center when not in verbose mode
    They are spammed by the server...

--- src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/block/chunk/ChunkCenterS2CP.kt
+++ src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/block/chunk/ChunkCenterS2CP.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -23,6 +23,7 @@ class ChunkCenterS2CP(buffer: PlayInByteBuffer) : PlayS2CPacket {
     private val position = ChunkPosition(buffer.readVarInt(), buffer.readVarInt())
 
     override fun log(reducedLog: Boolean) {
+        if (reducedLog) return
         Log.log(LogMessageType.NETWORK_IN, level = LogLevels.VERBOSE) { "Chunk center (position=$position)" }
     }
 }
