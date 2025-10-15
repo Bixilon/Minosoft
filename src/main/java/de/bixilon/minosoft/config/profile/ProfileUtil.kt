bd1491b64ab38c36b831c6b805996300f29602ae
Moritz Zwerger <bixilon@bixilon.de>
2025-10-15 13:49:04 +0200

    profiles: allow dot in name

--- src/main/java/de/bixilon/minosoft/config/profile/ProfileUtil.kt
+++ src/main/java/de/bixilon/minosoft/config/profile/ProfileUtil.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -14,7 +14,7 @@
 package de.bixilon.minosoft.config.profile
 
 object ProfileUtil {
-    val NAME_REGEX = "[\\w_ ]{1,32}".toRegex()
+    val NAME_REGEX = "[\\w_ .]{1,32}".toRegex()
 
     fun String.isValidName(): Boolean {
         return NAME_REGEX.matches(this)
