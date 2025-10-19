7dafc527f892648541b598292a3519fb306fcc8b
Moritz Zwerger <bixilon@bixilon.de>
2025-10-19 14:55:34 +0200

    log: properly hide slf4j eror message

--- src/main/java/de/bixilon/minosoft/gui/rendering/RenderContext.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/RenderContext.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2024 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -62,7 +62,7 @@ class RenderContext(
 
     val skeletal = SkeletalManager(this)
 
-    lateinit var renderStats: AbstractRenderStats
+    var renderStats: AbstractRenderStats = RenderStats()
         private set
 
     var font: FontManager = unsafeNull()
--- src/main/java/de/bixilon/minosoft/util/logging/LogPrintStream.kt
+++ src/main/java/de/bixilon/minosoft/util/logging/LogPrintStream.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2022 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -20,21 +20,13 @@ class LogPrintStream(
 ) : PrintStream(nullOutputStream()) {
 
     override fun print(string: String?) {
-        if (string == null) {
-            return
-        }
-        if (string.startsWith("SLF4J: ")) {
-            return
-        }
-        if (string.startsWith("ERROR StatusLogger Log4j2")) {
-            return
-        }
-        if (string.startsWith("Unknown element") && string.endsWith("):")) {
-            return
-        }
-        if (Thread.currentThread().name == "JavaFX-Launcher") {
-            return
-        }
+        if (string == null || string.isBlank()) return
+        if (string.startsWith("SLF4J: ") || string.startsWith("SLF4J(W):")) return
+        if (string.startsWith("ERROR StatusLogger Log4j2")) return
+        if (string.startsWith("Unknown element") && string.endsWith("):")) return
+
+        if (Thread.currentThread().name == "JavaFX-Launcher") return
+
         Log.log(message = string, type = type, level = level)
     }
 
