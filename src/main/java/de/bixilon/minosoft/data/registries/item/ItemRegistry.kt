ce66b31d1c898e3457e7e685a833aa18bd4dd8e3
Moritz Zwerger <bixilon@bixilon.de>
2025-10-22 09:48:10 +0200

    item registry: forbid air as item
    This is just on protocol level, this should get implemented on ItemStack level too

--- src/main/java/de/bixilon/minosoft/data/registries/item/ItemRegistry.kt
+++ src/main/java/de/bixilon/minosoft/data/registries/item/ItemRegistry.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -14,12 +14,14 @@
 package de.bixilon.minosoft.data.registries.item
 
 import de.bixilon.kutil.cast.CastUtil.unsafeCast
+import de.bixilon.minosoft.data.registries.blocks.types.air.AirBlock
 import de.bixilon.minosoft.data.registries.item.factory.ItemFactories
 import de.bixilon.minosoft.data.registries.item.factory.ItemFactory
 import de.bixilon.minosoft.data.registries.item.items.Item
 import de.bixilon.minosoft.data.registries.item.items.pixlyzer.PixLyzerItem
 import de.bixilon.minosoft.data.registries.registries.registry.MetaTypes
 import de.bixilon.minosoft.data.registries.registries.registry.Registry
+import de.bixilon.minosoft.protocol.protocol.ProtocolDefinition
 
 class ItemRegistry(
     parent: Registry<Item>? = null,
@@ -28,16 +30,23 @@ class ItemRegistry(
 
     override fun getOrNull(id: Int): Item? {
         if (flattened) {
+            if (id == ProtocolDefinition.AIR_BLOCK_ID) return null
             return super.getOrNull(id)
         }
         val itemId = id ushr 16
+        if (itemId shr 4 == ProtocolDefinition.AIR_BLOCK_ID) return null
+
         val meta = id and 0xFFFF
 
         var versionItemId = itemId shl 16
         if (meta > 0) {
             versionItemId = versionItemId or meta
         }
-        return super.getOrNull(versionItemId) ?: super.getOrNull(itemId shl 16) // ignore meta?
+        val item = super.getOrNull(versionItemId) ?: super.getOrNull(itemId shl 16) // ignore meta?
+
+        if (item?.identifier == AirBlock.Air.identifier) return null // TODO: use AirItem
+
+        return item
     }
 
     operator fun <T : Item> get(factory: ItemFactory<T>): T? {
