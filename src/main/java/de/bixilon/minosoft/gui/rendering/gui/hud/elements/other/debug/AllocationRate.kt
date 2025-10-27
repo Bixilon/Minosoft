6ac7551b4e6ddddb697959fea6a27c157006b021
Moritz Zwerger <bixilon@bixilon.de>
2025-10-27 17:39:59 +0100

    allocation rate: measure every second
    Otherwise the values jump up and down and really don't make sense

--- src/main/java/de/bixilon/minosoft/gui/rendering/gui/hud/elements/other/debug/AllocationRate.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/gui/hud/elements/other/debug/AllocationRate.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2023 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -15,17 +15,16 @@ package de.bixilon.minosoft.gui.rendering.gui.hud.elements.other.debug
 
 import de.bixilon.kutil.concurrent.schedule.RepeatedTask
 import de.bixilon.kutil.concurrent.schedule.TaskScheduler
-import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 object AllocationRate {
-    const val RUNS_PER_SECOND = 3
     private val RUNTIME = Runtime.getRuntime()
     var allocationRate = 0L
         private set
     private var previous = 0L
 
     init {
-        TaskScheduler += RepeatedTask(1000.milliseconds / RUNS_PER_SECOND) { tick() }
+        TaskScheduler += RepeatedTask(1.seconds) { tick() }
     }
 
     private fun tick() {
@@ -36,6 +35,6 @@ object AllocationRate {
             // gc was active
             return
         }
-        this.allocationRate = (allocated - previous) * RUNS_PER_SECOND
+        this.allocationRate = allocated - previous
     }
 }
