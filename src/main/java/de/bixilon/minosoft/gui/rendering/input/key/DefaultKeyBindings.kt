f59c4f8dcc87380c6d865c893734fa1ded7cfe03
Moritz Zwerger <bixilon@bixilon.de>
2025-11-14 13:44:56 +0100

    take screenshot at end of frame
    Otherwise it is just black

--- src/main/java/de/bixilon/minosoft/gui/rendering/input/key/DefaultKeyBindings.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/input/key/DefaultKeyBindings.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2024 Moritz Zwerger
+ * Copyright (C) 2020-2025 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -32,7 +32,7 @@ object DefaultKeyBindings {
         bindings.register(SCREENSHOT, KeyBinding(
             KeyActions.PRESS to setOf(KeyCodes.KEY_F2),
             ignoreConsumer = true,
-        )) { context.screenshotTaker.takeScreenshot() }
+        )) { context.queue += { context.screenshotTaker.takeScreenshot() } }
 
 
         bindings.register(FULLSCREEN, KeyBinding(
