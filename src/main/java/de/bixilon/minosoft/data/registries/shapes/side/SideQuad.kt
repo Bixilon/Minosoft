d8067206364bbf6196d1531f2a98458358f122fe
Moritz Zwerger <bixilon@bixilon.de>
2025-12-05 14:47:23 +0100

    remove a lot of old voxel side code
    Not needed anymore.

--- src/main/java/de/bixilon/minosoft/data/registries/shapes/side/SideQuad.kt
+++ src/main/java/de/bixilon/minosoft/data/registries/shapes/side/SideQuad.kt
@@ -20,71 +20,9 @@ data class SideQuad(
     val min: Vec2f,
     val max: Vec2f,
 ) {
-    private var hashCode = 0 // lazy
 
     constructor(minX: Float, minZ: Float, maxX: Float, maxZ: Float) : this(Vec2f(minOf(minX, maxX), minOf(minZ, maxZ)), Vec2f(maxOf(minX, maxX), maxOf(minZ, maxZ)))
-    constructor(minX: Double, minZ: Double, maxX: Double, maxZ: Double) : this(Vec2f(minOf(minX, maxX).toFloat(), minOf(minZ, maxZ).toFloat()), Vec2f(maxOf(minX, maxX).toFloat(), maxOf(minZ, maxZ).toFloat()))
 
-
-    fun touches(other: SideQuad): Boolean {
-        return !(this.min.x > other.max.x || other.min.x > this.max.x || this.min.y > other.max.y || other.min.y > this.max.y)
-    }
-
-    infix operator fun minus(set: VoxelSide): VoxelSide? {
-        val result: MutableSet<SideQuad> = ObjectOpenHashSet(set.sides.size)
-
-        for (side in set.sides) {
-            result += (this minus side)?.sides ?: continue
-        }
-
-        if (result.isEmpty()) return null
-
-        return VoxelSide(result)
-    }
-
-    infix operator fun minus(other: SideQuad): VoxelSide? {
-        val result: MutableSet<SideQuad> = ObjectOpenHashSet(4)
-
-
-        if (other.min.x > min.x && other.min.x < max.x) {
-            result += SideQuad(min.x, min.y, other.min.x, max.y)
-        }
-        if (other.min.y > min.y && other.min.y < max.y) {
-            result += SideQuad(min.x, min.y, max.x, other.min.y)
-        }
-
-        if (max.x > other.max.x) {
-            result += SideQuad(other.max.x, min.y, max.x, max.y)
-        }
-        if (max.y > other.max.y) {
-            result += SideQuad(min.x, other.max.y, max.x, max.y)
-        }
-
-        if (result.isEmpty()) return null
-
-
-        return VoxelSide(result)
-    }
-
-    private fun _hashCode(): Int {
-        var result = 1
-        result = 31 * result + min.hashCode()
-        result = 31 * result + max.hashCode()
-        return result
-    }
-
-    override fun hashCode(): Int {
-        if (hashCode == 0) {
-            hashCode = _hashCode()
-        }
-        return hashCode
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is SideQuad) return false
-        if (hashCode != other.hashCode) return false
-        return min == other.min && max == other.max
-    }
 
     fun surfaceArea(): Float {
         val surface = (max.x - min.x) * (max.y - min.y)
