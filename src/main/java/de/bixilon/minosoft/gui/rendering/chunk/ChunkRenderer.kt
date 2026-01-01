d14e8070be21f91b7f7222ac32dd08f9eaa94d47
Moritz Zwerger <bixilon@bixilon.de>
2026-01-01 20:04:14 +0100

    remove frame time burning
    great idea, unfortunately this is nuts. It makes the whole cpu block, all the render queries could already be collected.

--- src/main/java/de/bixilon/minosoft/gui/rendering/chunk/ChunkRenderer.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/chunk/ChunkRenderer.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -205,10 +205,6 @@ class ChunkRenderer(
     override fun postPrepareDraw() {
         context.profiler("unloading") { unloadingQueue.work() }
         context.profiler("loading") { loadingQueue.work() }
-    }
-
-    override fun burnTime(): Boolean {
-        return loadingQueue.burn()
     }
 
     override fun postDraw() {
