105dbe714f126766a19795f173b26f633bcb408c
Moritz Zwerger <bixilon@bixilon.de>
2026-01-04 20:54:25 +0100

    port GpuBufferStates::INITIALIZING
    Bad cherry pick...

--- src/main/java/de/bixilon/minosoft/gui/rendering/system/base/buffer/GpuBufferStates.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/system/base/buffer/GpuBufferStates.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -15,6 +15,7 @@ package de.bixilon.minosoft.gui.rendering.system.base.buffer
 
 enum class GpuBufferStates {
     PREPARING,
+    INITIALIZING,
     INITIALIZED,
     UNLOADED,
     ;
