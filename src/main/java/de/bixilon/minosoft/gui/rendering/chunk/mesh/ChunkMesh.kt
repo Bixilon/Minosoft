097d85f9ca9f021a8fad3a733bec1d970d3d874a
Moritz Zwerger <bixilon@bixilon.de>
2026-01-05 15:09:34 +0100

    chunk mesh: ignore if less then 10 fragments are shown

--- src/main/java/de/bixilon/minosoft/gui/rendering/chunk/mesh/ChunkMesh.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/chunk/mesh/ChunkMesh.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -51,7 +51,7 @@ class ChunkMesh(
         if (query.recordings == 0) return
 
         query.collect()
-        occlusion = if (query.result > 0) OcclusionStates.VISIBLE else OcclusionStates.INVISIBLE
+        occlusion = if (query.result > 10) OcclusionStates.VISIBLE else OcclusionStates.INVISIBLE
     }
 
     override fun unload() {
