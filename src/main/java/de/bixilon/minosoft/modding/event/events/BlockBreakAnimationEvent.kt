8c026efe3519d602794cb8e604eaa6b5eebd730b
Moritz Zwerger <bixilon@bixilon.de>
2026-01-05 16:22:00 +0100

    network: cleanup block break animation

--- src/main/java/de/bixilon/minosoft/modding/event/events/BlockBreakAnimationEvent.kt
+++ src/main/java/de/bixilon/minosoft/modding/event/events/BlockBreakAnimationEvent.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -15,14 +15,10 @@ package de.bixilon.minosoft.modding.event.events
 import de.bixilon.minosoft.data.world.positions.BlockPosition
 import de.bixilon.minosoft.modding.event.events.session.play.PlaySessionEvent
 import de.bixilon.minosoft.protocol.network.session.play.PlaySession
-import de.bixilon.minosoft.protocol.packets.s2c.play.block.BlockBreakAnimationS2CP
 
 class BlockBreakAnimationEvent(
     session: PlaySession,
-    val entityId: Int,
-    val blockPosition: BlockPosition,
-    val stage: Int,
-) : PlaySessionEvent(session), CancelableEvent {
-
-    constructor(session: PlaySession, packet: BlockBreakAnimationS2CP) : this(session, packet.entityId, packet.blockPosition, packet.stage)
-}
+    val id: Int,
+    val position: BlockPosition,
+    val progress: Float,
+) : PlaySessionEvent(session), CancelableEvent
--- src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/block/BlockBreakAnimationS2CP.kt
+++ src/main/java/de/bixilon/minosoft/protocol/packets/s2c/play/block/BlockBreakAnimationS2CP.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -12,7 +12,6 @@
  */
 package de.bixilon.minosoft.protocol.packets.s2c.play.block
 
-import de.bixilon.minosoft.data.world.positions.BlockPosition
 import de.bixilon.minosoft.modding.event.events.BlockBreakAnimationEvent
 import de.bixilon.minosoft.protocol.network.session.play.PlaySession
 import de.bixilon.minosoft.protocol.packets.s2c.PlayS2CPacket
@@ -23,32 +22,21 @@ import de.bixilon.minosoft.util.logging.LogLevels
 import de.bixilon.minosoft.util.logging.LogMessageType
 
 class BlockBreakAnimationS2CP(buffer: PlayInByteBuffer) : PlayS2CPacket {
-    /**
-     * Entity id of the entity who is breaking the block
-     */
-    val entityId: Int = buffer.readVarInt()
-    var blockPosition: BlockPosition = if (buffer.versionId < ProtocolVersions.V_14W03B) {
-        buffer.readIntBlockPosition()
-    } else {
-        buffer.readBlockPosition()
-    }
-    val stage: Int = let {
-        val value = buffer.readUnsignedByte()
-        when {
-            value < 0 -> -1
-            value > 8 -> -1
-            else -> value
-        }
-    }
+    val id = buffer.readVarInt()
+    var position = if (buffer.versionId < ProtocolVersions.V_14W03B) buffer.readIntBlockPosition() else buffer.readBlockPosition()
+    val stage = buffer.readUnsignedByte()
 
     override fun handle(session: PlaySession) {
-        val event = BlockBreakAnimationEvent(session, this)
-        if (session.events.fire(event)) {
-            return
-        }
+        val progress = stage / VANILLA_STAGES
+
+        session.events.fire(BlockBreakAnimationEvent(session, id, position, progress))
     }
 
     override fun log(reducedLog: Boolean) {
-        Log.log(LogMessageType.NETWORK_IN, level = LogLevels.VERBOSE) { "Block break animation (entityId=$entityId, blockPosition=$blockPosition, stage=$stage)" }
+        Log.log(LogMessageType.NETWORK_IN, level = LogLevels.VERBOSE) { "Block break animation (id=$id, position=$position, stage=$stage)" }
+    }
+
+    companion object {
+        const val VANILLA_STAGES = 8.0f
     }
 }
