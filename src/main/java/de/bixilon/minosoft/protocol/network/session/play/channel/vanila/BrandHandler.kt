59c343d7b412433bbc310d8eb3f7dfedaabaa4f7
Moritz Zwerger <bixilon@bixilon.de>
2026-01-17 15:47:50 +0100

    brand: send without length prefix

--- src/main/java/de/bixilon/minosoft/protocol/network/session/play/channel/vanila/BrandHandler.kt
+++ src/main/java/de/bixilon/minosoft/protocol/network/session/play/channel/vanila/BrandHandler.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2024 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -51,7 +51,7 @@ object BrandHandler {
     ) : PlayChannelHandler {
 
         override fun handle(buffer: PlayInByteBuffer) {
-            session.serverInfo.brand = buffer.readString()
+            session.serverInfo.brand = buffer.readString(buffer.size)
         }
     }
 }
