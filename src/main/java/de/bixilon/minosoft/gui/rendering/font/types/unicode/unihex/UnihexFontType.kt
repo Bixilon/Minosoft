f507584e33aef5e63eac46a604f8f032662b8e50
Gioyous The II <244157479+gioyous-it@users.noreply.github.com>
2026-08-28 22:37:24 +0200

    Update copyright year and format license comment
    Updated copyright year from 2025 to 2026 and improved formatting in the license comment.

--- src/main/java/de/bixilon/minosoft/gui/rendering/font/types/unicode/unihex/UnihexFontType.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/font/types/unicode/unihex/UnihexFontType.kt
@@ -1,14 +1,22 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
- * This software is not affiliated with Mojang AB, the original developer of Minecraft.
+ * This software is not affiliated with Mojang AB, the original developer
+ * of Minecraft.
  */
 
 package de.bixilon.minosoft.gui.rendering.font.types.unicode.unihex
@@ -120,17 +128,18 @@ class UnihexFontType(
             return array
         }
 
-       private fun InputStream.readUnihex(chars: Int2ObjectOpenHashMap<ByteArray>): Int {
-           val buffer = ByteArray(64)
-           var totalWidth = 0
-           while (this.available() > 0) {
-               val codePoint = readHexInt()
-               if (codePoint == -1) continue
-               val data = this.readUnihexData(buffer)
-               if (chars.putIfAbsent(codePoint, data) == null) {
-                   totalWidth += data.size / (UnifontRasterizer.HEIGHT / Byte.SIZE_BITS)
-               }
-           }
+        private fun InputStream.readUnihex(chars: Int2ObjectOpenHashMap<ByteArray>): Int {
+            val buffer = ByteArray(64)
+            var totalWidth = 0
+            while (this.available() > 0) {
+                val codePoint = readHexInt()
+                if (codePoint == -1) continue
+                val data = readUnihexData(buffer)
+                if (chars.putIfAbsent(codePoint, data) == null) {
+                    totalWidth += data.size / (UnifontRasterizer.HEIGHT / Byte.SIZE_BITS)
+                }
+            }
 
-           return totalWidth
-       }
+            return totalWidth
+        }
+    }
